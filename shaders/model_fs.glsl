00aa896a50669e92020938b8c974241342e19bc5
Thomas Aven <avensin@hotmail.com>
2018-02-10 17:42:03 +0100

    Fix texturing of models in shader.

--- shaders/model_fs.glsl
+++ shaders/model_fs.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 uniform sampler2D texture_diffuse1;
 
-in vec2 texture_coords;
+in vec2 texture_coord;
 
 out vec4 fragment_color;
 
 void main()
 {
-	fragment_color = texture(texture_diffuse1, texture_coords);
+	fragment_color = texture(texture_diffuse1, texture_coord);
 }
